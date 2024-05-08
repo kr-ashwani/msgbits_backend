28469781d0fa0e8a6e52db3bc55a186225189cf5
Ashwani Kumar <a61ashwanikumar@gmail.com>
2024-05-08 20:56:25 +0530

    mail ejs added

--- errors/loggerError/loggerDBError.ts
+++
@@ -1,9 +0,0 @@
-import BaseError from "../BaseError";
-
-class loggerDBError extends BaseError {
-  constructor(err: Error) {
-    super(err.message, loggerDBError.name, true, err.stack);
-    Object.setPrototypeOf(this, new.target.prototype);
-  }
-}
-export default loggerDBError;
--- errors/loggerError/loggerFileError.ts
+++
@@ -1,9 +0,0 @@
-import BaseError from "../BaseError";
-
-class loggerFileError extends BaseError {
-  constructor(err: Error) {
-    super(err.message, loggerFileError.name, true, err.stack);
-    Object.setPrototypeOf(this, new.target.prototype);
-  }
-}
-export default loggerFileError;
